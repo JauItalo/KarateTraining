e7441f84c14bd450caea8024d394c51410616cbb
Jau Italo <jau.batista@dcx.ufpb.br>
2025-03-20 17:43:59 -0300

    enfim, o ultimo desse crud,pode deletar um treino

--- controllers/trainingController.js
+++ controllers/trainingController.js
@@ -50,5 +50,15 @@ exports.updateTraining  = async (req, res) => {
     }
 };
 
-
+exports.deleteTraining = async (req, res) => {
+    try {
+        const training = await Training.findByIdAndDelete(req.params.id);
+        if (!training) {
+            return res.status(404).json({ error: 'Training not found'});
+        }
+        res.status(200).json({message: 'Training deleted'});
+    }   catch (err) {
+        res.status(400).json({ error: err.message});
+    }
+};
 
