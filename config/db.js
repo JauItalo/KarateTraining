bf8584d7efeed105224900e4c736718d72d8f20a
Jau Italo <jau.batista@dcx.ufpb.br>
2025-03-21 17:04:09 -0300

    back-end da quase finalizado

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const conncetDB = async () => {
+const connectDB = async () => {
     try{
         await mongoose.connect('mongodb://localhost:27017/karate-training',
 {
@@ -9,11 +9,11 @@ const conncetDB = async () => {
 
 
     });
-    console.log('MongoDB conneceted');
+    console.log('MongoDB connected');
     } catch (err) {
-        console.error('MongoDB conncection error:', err);
+        console.error('MongoDB connection error:', err);
         process.exit(1);
     }
 };
 
-module.exports = conncetDB;
+module.exports = connectDB;
---
+++ routes/app.js
@@ -0,0 +1,23 @@
+
+
+express = require ('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const connectDB = require('../config/db');
+const trainingRoutes = require ('../routes/trainingRoutes');
+
+
+const app = express();
+
+
+connectDB()
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.use('/api/trainings', trainingRoutes);
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log('Server running on port ${PORT}'));
