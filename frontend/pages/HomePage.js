f7b4ec4f3ef070cdb1144050b61985f28ac76d62
Jau Italo <jau.batista@dcx.ufpb.br>
2025-03-24 13:48:57 -0300

    de volta aos trabalhos

---
+++ frontend/pages/HomePage.js
@@ -0,0 +1,39 @@
+import React, { use, useEffect, useState } from 'react';
+import axios from 'axios';
+import TrainingForm from '../components/TrainingForm';
+
+
+const HomePage = () => {
+    const [trainings, setTrainings] = useState([]);
+
+    useEffect(() => {
+        const fetchTrainings = async () => {
+            try {
+                const res = await axios.get('http://localhost:5000/api/trainings');
+
+                setTrainings(res.data);
+            } catch (err) {
+                console.error(err);
+            }
+        };
+        fetchTrainings();
+    }, []);
+
+        return (
+            <div>
+                <h1>Karate Training App</h1>
+                <TrainingForm />
+                <ul>
+                    {trainings.map((training) => (
+                        <li key={training._id}>
+                            <strong>{training.type}</strong> - {training.duration} minutes
+                            <p>{training.notes}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
+
+
+export default HomePage;
